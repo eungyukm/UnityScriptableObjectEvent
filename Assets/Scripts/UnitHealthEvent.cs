cbeedaeb95646edced134687265c4a457915fe37
eungyukm <86348187+eungyukm@users.noreply.github.com>
2021-08-04 18:13:23 +0900

    event system
    event system

---
+++ Assets/Scripts/UnitHealthEvent.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnitHealthEvent : MonoBehaviour
+{
+    public FloatVariable hp;
+    public bool resetHP;
+    public FloatReference startingHP;
+    public UnityEvent damageEvent;
+    public UnityEvent deathEvent;
+
+    private void Start()
+    {
+        if(resetHP)
+        {
+            hp.SetValue(startingHP);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ãæµ¹!");
+        DamageDealer damage = other.gameObject.GetComponent<DamageDealer>();
+        if(damage != null)
+        {
+            hp.ApplyChange(-damage.damageAmount);
+            damageEvent.Invoke();
+        }
+
+        if(hp.value <= 0.0f)
+        {
+            deathEvent.Invoke();
+        }
+    }
+}
